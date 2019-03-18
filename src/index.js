d6fbba1161626393ab7814d33d129f46583392ac
VladKhrlmv <kharlamov-vlad03@mail.ru>
2019-03-18 17:18:42 +0300

    feat: count zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let zeroCounter = 3e9;
+  for (let i = 2; i <= base; i++) {
+
+    if (base % i == 0) {
+      let degreeOfEntry = 0;
+
+      while (base % i == 0) {
+        degreeOfEntry++;
+        base /=i;
+      }
+
+      let numberOfEntry = 0;
+      let variable2 = number;
+      let deg = i;
+      while (variable2 / deg >= 1) {
+        numberOfEntry += Math.floor(variable2 / deg);
+        deg*= i;
+      }
+
+      if ( zeroCounter > Math.floor(numberOfEntry / degreeOfEntry)) {
+        zeroCounter = Math.floor(numberOfEntry / degreeOfEntry);
+      }
+    }
+
+  }
+  return zeroCounter;
 }
